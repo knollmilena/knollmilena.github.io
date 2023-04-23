8b7f02552447951cdea8f4da6db3d3bbb39de8db
knollmilena <50546237+knollmilena@users.noreply.github.com>
2023-04-23 19:27:39 +0300

    formatting

--- feedback-form/js/script.js
+++ feedback-form/js/script.js
@@ -5,28 +5,29 @@ const inputEmail = document.querySelector('.input__mail');
 let result = true;
 
 function validation(input) {
-    if(input.value == ""){
-      input.classList.add('form__error');
-      input.classList.remove('input');
-      result = false;
-    } else {
-      input.classList.remove('form__error');
-      input.classList.add('input');
-    }
-   return result
+  if (input.value == '') {
+    input.classList.add('form__error');
+    input.classList.remove('input');
+    result = false;
+  } else {
+    input.classList.remove('form__error');
+    input.classList.add('input');
+  }
+  return result;
 }
 
-inputs.forEach(input => input.addEventListener('input', function(){
-  validation(input);
-  }))
+inputs.forEach((input) =>
+  input.addEventListener('input', function () {
+    validation(input);
+  })
+);
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
   event.preventDefault();
-  inputs.forEach(input => validation(input));
+  inputs.forEach((input) => validation(input));
   if (result == true) {
-      alert('Отправено!');
-    } else {
-      alert('Заполните все поля!');
-    }
-  });
-
+    alert('Отправено!');
+  } else {
+    alert('Заполните все поля!');
+  }
+});
