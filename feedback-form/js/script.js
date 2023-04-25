3c30e7b58b2d736a3f67487ab51744e371c7e16c
knollmilena <50546237+knollmilena@users.noreply.github.com>
2023-04-25 18:36:52 +0300

    done

--- feedback-form/js/script.js
+++ feedback-form/js/script.js
@@ -2,9 +2,12 @@ const form = document.getElementById('form');
 const button = document.getElementById('send_mess');
 const inputs = document.querySelectorAll('.input');
 const inputEmail = document.querySelector('.input__mail');
-let result = true;
+const optionItemOther = document.querySelector('.form__theme-other');
+const formTheme = document.querySelector('.form__theme');
+
 
 function validation(input) {
+  let result = true;
   if (input.value == '') {
     input.classList.add('form__error');
     input.classList.remove('input');
@@ -22,9 +25,40 @@ inputs.forEach((input) =>
   })
 );
 
+formTheme.addEventListener('change', function (event){
+  const wrapInputTheme = document.createElement('li')
+  wrapInputTheme.classList.add('form__item', 'input__theme');
+
+  const labelTextOther = document.createElement('label')
+  labelTextOther.classList.add('form__text');
+  labelTextOther.textContent = 'Какая тема?';
+
+  const inputTheme = document.createElement('input');
+  inputTheme.classList.add('input__theme', 'input');
+  inputTheme.placeholder = 'Расскажите подробнее';
+  inputTheme.type = "text";
+
+  let indexOption = formTheme.selectedIndex;
+  let options = formTheme.options;
+
+  let parentElem = formTheme.parentElement;
+  let nextWrapTheme = parentElem.nextSibling;
+
+if(!formTheme.parentElement.nextElementSibling.classList.contains('input__theme') && indexOption === options.length-1){
+    parentElem.insertAdjacentElement('afterEnd', wrapInputTheme);
+    wrapInputTheme.append(labelTextOther);
+    wrapInputTheme.append(inputTheme);
+
+  }
+  if ((indexOption !== 2) && formTheme.parentElement.nextElementSibling.classList.contains('input__theme')) {
+    nextElem = parentElem.nextElementSibling;
+    nextElem.remove();
+  }
+});
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  inputs.forEach((input) => validation(input));
+  let result;
+  inputs.forEach((input) => result = validation(input));
   if (result == true) {
     alert('Отправено!');
   } else {
