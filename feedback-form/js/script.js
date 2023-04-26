8e1664bc50c3a9f9705c717ea9743e03688914e9
knollmilena <50546237+knollmilena@users.noreply.github.com>
2023-04-27 01:08:28 +0300

    fixed bags

--- feedback-form/js/script.js
+++ feedback-form/js/script.js
@@ -1,10 +1,20 @@
 const form = document.getElementById('form');
 const button = document.getElementById('send_mess');
-const inputs = document.querySelectorAll('.input');
+// const inputs = document.querySelectorAll('.input');
+const liveInputs = document.getElementsByClassName('input');
 const inputEmail = document.querySelector('.input__mail');
 const optionItemOther = document.querySelector('.form__theme-other');
 const formTheme = document.querySelector('.form__theme');
 
+
+
+// function generationLiveInputs(){
+//   if (formTheme.parentElement.nextElementSibling.classList.contains('input__theme')) {
+//     liveInputs = document.getElementsByClassName('input');
+//     inputs = Array.from(liveInputs);
+//   }
+//   return inputs;
+// }
 
 function validation(input) {
   let result = true;
@@ -15,15 +25,16 @@ function validation(input) {
   } else {
     input.classList.remove('form__error');
     input.classList.add('input');
+    result = true;
   }
   return result;
 }
 
-inputs.forEach((input) =>
-  input.addEventListener('input', function () {
-    validation(input);
-  })
-);
+// form.addEventListener('input', function () {
+//   inputs.forEach((input) => validation(input));
+//   console.log('fsdfsd');
+// });
+
 
 formTheme.addEventListener('change', function (event){
   const wrapInputTheme = document.createElement('li')
@@ -48,20 +59,45 @@ if(!formTheme.parentElement.nextElementSibling.classList.contains('input__theme'
     parentElem.insertAdjacentElement('afterEnd', wrapInputTheme);
     wrapInputTheme.append(labelTextOther);
     wrapInputTheme.append(inputTheme);
-
   }
   if ((indexOption !== 2) && formTheme.parentElement.nextElementSibling.classList.contains('input__theme')) {
     nextElem = parentElem.nextElementSibling;
     nextElem.remove();
   }
+
 });
+
+
+// старый прослушиватель
+// inputs.forEach((input) =>
+//   input.addEventListener('input', function () {
+//     validation(input);
+//     console.log(generationLiveInputs());
+//   })
+// );
+
+
+
+
+form.addEventListener('input', function(){
+  for (let input of liveInputs) {
+    input.addEventListener('input', function () {
+          validation(input);
+        })
+  }
+})
+
+
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   let result;
-  inputs.forEach((input) => result = validation(input));
-  if (result == true) {
-    alert('Отправено!');
-  } else {
-    alert('Заполните все поля!');
+  const inputs = document.querySelectorAll('.input');
+  inputs.forEach((input) => validation(input));
+  for (i = 0; i < inputs.length; ++i){
+    result = validation(inputs[i]);
+    if (result === false) {
+      alert ('Заполните все поля!');
+      break
+    }
   }
 });
